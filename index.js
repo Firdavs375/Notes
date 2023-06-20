198d35421495348fd86b4d4f12c9fc03770f0297
Firdavs375 <firdavss.3755@gmail.com>
2023-06-20 18:00:30 +0500

    flex

---
+++ index.js
@@ -0,0 +1,43 @@
+const center = document.querySelector(".center")
+const btn = document.querySelector("button")
+const note = document.querySelectorAll(".container")
+const delate = document.querySelector(".img")
+
+
+function showNotes() {
+    center.innerHTML = localStorage.getItem("container")
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("container", center.innerHTML);
+}
+
+
+btn.onclick = () => {
+    center.innerHTML += `
+    <div class="container">
+    <img class="img" src="./img/delate.png" alt="png">
+    <div class="minBox"></div>
+    <p contenteditable="true" class="inpBox"></p>
+    </div>
+    `
+};
+
+
+center.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        note = document.querySelectorAll(".inpBox");
+        note.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+
+})
+
